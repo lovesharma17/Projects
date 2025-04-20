200daa6548756a98a7592200ac80ea1c091a6b0e
lovesharma17 <125554675+lovesharma17@users.noreply.github.com>
2025-04-20 11:26:01 +0530

    Add files via upload

---
+++ musiclib.py
@@ -0,0 +1,13 @@
+
+
+
+music = {
+    "date" : "https://www.youtube.com/watch?v=Nxs_mpWt2BA&pp=ygUJcGxheWFkYXRl",
+
+    "astronaut" : "http://www.youtube.com/watch?v=MEg-oqI9qmw&pp=ygUWYXN0cm9uYXV0IGluIHRoZSBvY2Vhbg%3D%3D",
+
+    "saajna" : "https://www.youtube.com/watch?v=mYBqt29G9cA&pp=ygUGc2Fham5h"  ,
+
+    "sajna" : "https://www.youtube.com/watch?v=mYBqt29G9cA&pp=ygUGc2Fham5h" 
+    
+    }
